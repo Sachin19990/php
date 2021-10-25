f5d26f46c9ed4e3f1dc6601af54b9bb182650720
Sachin19990 <sshanky19990@gmail.com>
2021-10-25 15:16:37 +0530

    changes

---
+++ component/footer.php
@@ -0,0 +1,12 @@
+<div class="container-fluid bg-dark text-white">
+    <footer class="py-3 mt-2">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item"><a class="nav-link px-2 text-white" href="javascript:void(0)">Home</a></li>
+            <li class="nav-item"><a class="nav-link px-2 text-white" href="javascript:void(0)">Feature</a></li>
+            <li class="nav-item"><a class="nav-link px-2 text-white" href="javascript:void(0)">pricing</a></li>
+            <li class="nav-item"><a class="nav-link px-2 text-white" href="javascript:void(0)">About</a></li>
+            <li class="nav-item"><a class="nav-link px-2 text-white" href="javascript:void(0)">FAQ</a></li>
+        </ul>
+        <p class="text-center">&copy; 2021 Company, Inc</p>
+    </footer>
+</div>
